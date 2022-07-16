ff6a7d2357fc2979204f1f77c4b0d1d2a12c286d
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-07-15 21:11:40 -0500

    Cell collapse functions

--- sketch.js
+++ sketch.js
@@ -14,6 +14,38 @@ const RIGHT = 2;
 const DOWN  = 3;
 const LEFT  = 4;
 
+/////FUNCTIONS
+function createGrid(){
+  for(let i = 0; i < DIM * DIM; i++){
+    grid[i]= {
+      collapsed: false,
+      options: [BLANCK,UP,RIGHT,DOWN,LEFT]
+    }
+  }
+}
+
+function getCellWithLeastEntropy(){
+  //Cell with the least entropy
+  gridCopy = grid.slice();  //Copies the grid
+  gridCopy.sort((a,b) => {
+    return a.options.length - b.options.length;
+  })
+  let minEntropy = gridCopy[0].options.length
+  gridCopy = gridCopy.filter(min => min.options.length == minEntropy)
+  return gridCopy;
+}
+
+function collapseCell(){
+    //Cell with the least entropy
+    gridCopy              = getCellWithLeastEntropy();    //Gets the group of the cells with the least entropy
+    const cellChosen      = random(gridCopy);             //Picks a random cell from the group
+    cellChosen.collapsed  = true;                         //Collapses the cell
+    const pick            = random(cellChosen.options);   //Choses an option from the options pool
+    cellChosen.options    = [pick];                       //Assigns the iption to the hosen cell
+  
+}
+///ALGORITHM
+
 function preload() {
   //Loading the tile images
   tiles[0] = loadImage("Tiles/Corners/00_Empty.png")
@@ -26,20 +58,38 @@ function preload() {
 function setup(){
   createCanvas(400,400);
 
-  //All sells star in an inactive state
-  for(let i = 0; i < DIM * DIM; i++){
-    grid[i]= {
-      collapsed: false,
-      options: [BLANCK,UP,RIGHT,DOWN,LEFT]
-    }
-  }
+  //All cells star in an inactive state
+  createGrid();
+  
 }
 
 function draw() {
   background(0);
-  image(tiles[0], 0, 0);
-  image(tiles[1], 10 , 0);
-  image(tiles[2], 20, 0);
-  image(tiles[3], 10, 10);
-  image(tiles[4], 0, 10);
+  
+  //Collapses the cell with the least entropy
+  collapseCell();
+
+
+  //Dimesion f the grid
+  const w = width  / DIM;
+  const h = height / DIM;
+  //Explores all the grid
+  for(let j = 0; j < DIM; j++){
+    for(let i = 0; i < DIM; i++ ){
+      let cell = grid[i + j * DIM]
+      //If the cell has been collapsed
+      if(cell.collapsed){
+        let index = cell.options[0];
+        image(tiles[index], i * w, j * h, w, h)
+      } else{
+        //If not paint a black rect
+        fill(0);
+        stroke(255);
+        rect(i * w, j * h, w , h)
+      }
+    }
+  }
+
+noLoop();
+
 }
