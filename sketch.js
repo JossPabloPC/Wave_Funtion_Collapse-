59986c0e4c3842af10479d60994a279b99322713
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-07-15 19:52:47 -0500

    Tile images loaded

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+/// <reference path="TSDef/p5.global-mode.d.ts" />
+
+//Tile images
 const tiles = [];
 
-function setup() {
-  tiles[0]
+function preload() {
+  //Loading the tile images
+  tiles[0] = loadImage("Tiles/Corners/00_Empty.png")
+  tiles[1] = loadImage("Tiles/Corners/01_Corner.png")
+  tiles[2] = loadImage("Tiles/Corners/02_Corner.png")
+  tiles[3] = loadImage("Tiles/Corners/03_Corner.png")
+  tiles[4] = loadImage("Tiles/Corners/04_Corner.png")
 }
 
 function setup(){
@@ -9,5 +17,10 @@ function setup(){
 }
 
 function draw() {
-  background(151);
+  background(0);
+  image(tiles[0], 0, 0);
+  image(tiles[1], 10 , 0);
+  image(tiles[2], 20, 0);
+  image(tiles[3], 10, 10);
+  image(tiles[4], 0, 10);
 }
