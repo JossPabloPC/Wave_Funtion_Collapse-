7ef5aea8b6607a6a6b501c020557f4113ef80793
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-07-15 20:07:47 -0500

    Cell states created

--- sketch.js
+++ sketch.js
@@ -3,17 +3,36 @@
 //Tile images
 const tiles = [];
 
+//State of each cell
+const grid  = [];
+const DIM   = 2;
+
+//Possible cell states 
+const BLANCK = 0;
+const UP    = 1;
+const RIGHT = 2;
+const DOWN  = 3;
+const LEFT  = 4;
+
 function preload() {
   //Loading the tile images
   tiles[0] = loadImage("Tiles/Corners/00_Empty.png")
-  tiles[1] = loadImage("Tiles/Corners/01_Corner.png")
-  tiles[2] = loadImage("Tiles/Corners/02_Corner.png")
-  tiles[3] = loadImage("Tiles/Corners/03_Corner.png")
-  tiles[4] = loadImage("Tiles/Corners/04_Corner.png")
+  tiles[1] = loadImage("Tiles/Corners/01_Up.png")
+  tiles[2] = loadImage("Tiles/Corners/02_Right.png")
+  tiles[3] = loadImage("Tiles/Corners/03_Down.png")
+  tiles[4] = loadImage("Tiles/Corners/04_Left.png")
 }
 
 function setup(){
   createCanvas(400,400);
+
+  //All sells star in an inactive state
+  for(let i = 0; i < DIM * DIM; i++){
+    grid[i]= {
+      collapsed: false,
+      options: [BLANCK,UP,RIGHT,DOWN,LEFT]
+    }
+  }
 }
 
 function draw() {
