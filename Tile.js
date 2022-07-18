18c19226d049988afcb47c3817f073c56e7fc528
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-07-18 12:42:45 -0500

    Create Tile.js

---
+++ Tile.js
@@ -0,0 +1,10 @@
+class Tile{
+    constructor(img, sockets, i){
+        this.img        = img;
+        this.sockets    = sockets;
+        this.up         = [];
+        this.right      = [];
+        this.down       = [];
+        this.left       = [];
+    }
+}
